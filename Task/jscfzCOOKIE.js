d9a5298d328088409c16cbb8d13d462a6852115f
pizile666 <82098259+pizile666@users.noreply.github.com>
2022-01-07 10:26:08 +0800

    Update jscfzCOOKIE.js

--- Task/jscfzCOOKIE.js
+++ Task/jscfzCOOKIE.js
@@ -1,16 +1,80 @@
-module.exports = {"id":"jscfz","name":"嘉实宠粉站","keys":["jscfzheader","jscfzheader2","jscfzheader3","jscfzheader4","jscfzheader5","jscfzheader6","jscfzheader7","jscfzheader8","jscfzheader9","jscfzheader10","jscfzheader11","jscfzheader12","jscfzheader13","jscfzheader14","jscfzheader15"],"author":"@tom","settings":[{"id":"jscfzSuffix","name":"当前账号","val":"1","type":"number","desc":"当前抓取ck记录的账号序号，如：1、2、3、4"},{"id":"jscfzCount","name":"账号个数","val":"3","type":"number","desc":"指定任务最多跑几个账号，根据抓取的账号数据个数来设值"},{"id":"jscfzXH","name":"循环获取ck","val":"0","type":"number","desc":"0关闭，1打开，默认关闭"},{"id":"jscfzTXTX","name":"txtx","val":"0","type":"number","desc":"0关闭，1打开，默认关闭"},{"id":"jscfzSC","name":"sc","val":"0","type":"number","desc":"0关闭，1打开，默认关闭"},{"id":"jscfznotifyttt","name":"推送控制","val":"1","type":"number","desc":"0关闭，1推送,默认12点以及23点推送"},{"id":"jscfznotifyInterval","name":"通知控制","val":"2","type":"number","desc":"0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"},{"id":"jscfzMinutes","name":"推送-通知 分钟控制","val":"10","type":"number","desc":"推送以及通知控制在什么分钟段，可设置0-59,默认0到10"}],"repo":"https://raw.githubusercontent.com/xl2101200/-/main/jscfz.js","icons":["https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg","https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg"],"script":"https://raw.githubusercontent.com/xl2101200/-/main/jscfz.js","icon":"https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg","favIcon":"mdi-star-outline","favIconColor":"grey","datas":[{"key":"jscfzheader","val":""},
-{"key":"jscfzheader2","val":""},
-{"key":"jscfzheader3","val":""},
-{"key":"jscfzheader4","val":""},
-{"key":"jscfzheader5","val":""},
-{"key":"jscfzheader6","val":""},
-{"key":"jscfzheader7","val":""},
-{"key":"jscfzheader8","val":""},
-{"key":"jscfzheader9","val":""},
-{"key":"jscfzheader10","val":""},
-{"key":"jscfzheader11","val":""},
-{"key":"jscfzheader12","val":""},
-{"key":"jscfzheader13","val":""},
-{"key":"jscfzheader14","val":""},
-{"key":"jscfzheader15","val":""}],
-"sessions":[],"isLoaded":true}
+module.exports = {
+    "id": "jscfz",
+    "name": "嘉实宠粉站",
+    "keys": ["jscfzheader","jscfzheader2","jscfzheader3","jscfzheader4","jscfzheader5","jscfzheader6","jscfzheader7","jscfzheader8","jscfzheader9","jscfzheader10","jscfzheader11","jscfzheader12","jscfzheader13","jscfzheader14","jscfzheader15","jscfzheader16","jscfzheader17","jscfzheader18","jscfzheader19","jscfzheader20"],
+    "author": "@tom",
+    "settings": [{
+      "id": "jscfzSuffix",
+      "name": "当前账号",
+      "val": "1",
+      "type": "number",
+      "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+    }, {
+      "id": "jscfzCount",
+      "name": "账号个数",
+      "val": "5",
+      "type": "number",
+      "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+    }, {
+      "id": "jscfzXH",
+      "name": "循环获取ck",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jscfzTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jscfzSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "jscfznotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "jscfznotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "jscfzMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+    }],
+    "repo": "https://raw.githubusercontent.com/xl2101200/-/main/jscfz.js",
+    "icons": ["https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg", "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg"],
+    "script": "https://raw.githubusercontent.com/xl2101200/-/main/jscfz.js",
+    "icon": "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg",
+    "favIcon": "mdi-star-outline",
+    "favIconColor": "grey",
+    "datas": [{
+      "key": "jscfzheader",
+      "val": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGlzIGlzIGxpdGVtYWxsIHRva2VuIiwiYXVkIjoiTUlOSUFQUCIsImlzcyI6IkxJVEVNQUxMIiwidXNlcklkIjoxNDU1MzAsImlhdCI6MTY0MTQ3MjQzNn0.xCIO7PtyRL6jshiMs6BhFpu85jqCm79FGvHOeLnLfpg"
+    }, {
+      "key": "jscfzheader2",
+      "val": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGlzIGlzIGxpdGVtYWxsIHRva2VuIiwiYXVkIjoiTUlOSUFQUCIsImlzcyI6IkxJVEVNQUxMIiwidXNlcklkIjoyMjY4MDEsImlhdCI6MTY0MTQ3MjU2MX0.VmOJLLWU5jYlPe6vKVHUgMmzPGPo4Sz91KXT-XSQbHY"
+    }, {
+      "key": "jscfzheader3",
+      "val": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGlzIGlzIGxpdGVtYWxsIHRva2VuIiwiYXVkIjoiTUlOSUFQUCIsImlzcyI6IkxJVEVNQUxMIiwidXNlcklkIjoyMjY4MDIsImlhdCI6MTY0MTQ3MjYyOH0.2T_4zEwOFIJJNYk0Jw0M7CAtVodX-CbiynN3b7yRj9I"
+    }, {
+      "key": "jscfzheader4",
+      "val": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGlzIGlzIGxpdGVtYWxsIHRva2VuIiwiYXVkIjoiTUlOSUFQUCIsImlzcyI6IkxJVEVNQUxMIiwidXNlcklkIjoyMjY4MDMsImlhdCI6MTY0MTQ3MjY4OH0.VMBB4QXso0OAoZP7rjvmHTNi-p4VK-EDew2rVEI69Zw"
+    }, {
+      "key": "jscfzheader5",
+      "val": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0aGlzIGlzIGxpdGVtYWxsIHRva2VuIiwiYXVkIjoiTUlOSUFQUCIsImlzcyI6IkxJVEVNQUxMIiwidXNlcklkIjoyMjY4MDQsImlhdCI6MTY0MTQ3Mjc2NH0.50t2pE6452ixhZ9onIWXZ4hacWkujhy8dhkdbMbZ5O8"
+    }],
+    "sessions": [],
+    "isLoaded": true
+  }
+  
